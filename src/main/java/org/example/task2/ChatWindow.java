16982d07f8586bd8488979064ea2d5d6b7b1108d
WaleriiChirkov-16021990 <zx84058405@live.com>
2023-10-04 13:41:41 +0300

    запись и чтение из файла реализованы

--- src/main/java/org/example/task2/ChatWindow.java
+++ src/main/java/org/example/task2/ChatWindow.java
@@ -4,6 +4,7 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
 
 //Создать окно клиента чата. Окно должно содержать JtextField для ввода логина, пароля, IP-адреса сервера, порта подключения к серверу, область ввода
 //        сообщений, JTextArea область просмотра сообщений чата и JButton подключения к серверу и отправки сообщения в чат. Желательно сразу сгруппировать
@@ -23,21 +24,26 @@ public class ChatWindow extends JFrame {
     private final JPanel panelBottom;
     private final JPanel panelBottomFromAreaText;
     private final JPanel panelBottomFromMessage;
-
+    private final JPanel panelAlarmMessage;
+    private final JLabel alarmLabel;
     private final JTextField textFieldMessage;
     private final JButton buttonSendMessage;
     private final AuthenticateServer authenticateServer;
+
+    private final File file = new File("history.txt");
+
     public ChatWindow(AuthenticateServer authenticateServer) throws HeadlessException {
         this.authenticateServer = authenticateServer;
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setBounds(WINDOW_POSX,WINDOW_POSY,WINDOW_WIDTH,WINDOW_HEIGHT);
+        setBounds(WINDOW_POSX, WINDOW_POSY, WINDOW_WIDTH, WINDOW_HEIGHT);
         setTitle("ServerChat");
         setResizable(false);
-        panelFullScreen = new JPanel(new GridLayout(2,1));
-        panelTop = new JPanel(new GridLayout(1,1));
-        panelBottom = new JPanel(new GridLayout(2,1));
-        panelBottomFromAreaText = new JPanel(new GridLayout(1,2));
-        panelBottomFromMessage = new JPanel(new GridLayout(1,2));
+        panelFullScreen = new JPanel(new GridLayout(2, 1));
+        panelTop = new JPanel(new GridLayout(1, 1));
+        panelBottom = new JPanel(new GridLayout(2, 1));
+        panelAlarmMessage = new JPanel(new GridLayout(2, 2));
+        panelBottomFromAreaText = new JPanel(new GridLayout(1, 2));
+        panelBottomFromMessage = new JPanel(new GridLayout(1, 2));
         jTextAreaMessages = new JTextArea();
         buttonSave = new JButton("Save");
         buttonLoaded = new JButton("Load");
@@ -46,15 +52,19 @@ public class ChatWindow extends JFrame {
         add(panelFullScreen);
         panelFullScreen.add(panelTop);
         panelFullScreen.add(panelTop);
-        panelFullScreen.add(panelBottom,BorderLayout.SOUTH);
+        panelFullScreen.add(panelBottom, BorderLayout.SOUTH);
         panelTop.add(jTextAreaMessages);
+        panelTop.add(panelAlarmMessage);
         panelBottomFromAreaText.add(buttonSave);
         panelBottomFromAreaText.add(buttonLoaded);
         panelBottom.add(panelBottomFromAreaText, BorderLayout.SOUTH);
         panelBottomFromMessage.add(textFieldMessage);
         panelBottomFromMessage.add(buttonSendMessage, BorderLayout.EAST);
         panelBottom.add(panelBottomFromMessage);
-
+        alarmLabel = new JLabel("Alarm");
+        panelAlarmMessage.setVisible(false);
+        panelAlarmMessage.add(alarmLabel, BorderLayout.WEST);
+        loadedHistoryFromFile();
         setVisible(true);
 
 
@@ -71,6 +81,62 @@ public class ChatWindow extends JFrame {
             }
         });
 
+        buttonSave.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (writeToHistory()) {
+                    panelAlarmMessage.setVisible(false);
+                } else {
+                    alarmLabel.setText("ALARM: History not saved");
+                    panelAlarmMessage.setVisible(true);
+                }
+            }
+        });
+
+        buttonLoaded.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (loadedHistoryFromFile()) {
+                    panelAlarmMessage.setVisible(false);
+                } else {
+                    alarmLabel.setText("ALARM: History not loaded");
+                    panelAlarmMessage.setVisible(true);
+                }
+            }
+        });
+
+    }
+
+    public boolean writeToHistory() {
+        try (FileWriter fileWriter = new FileWriter(file, true)) {
+            fileWriter.write(jTextAreaMessages.getText());
+            fileWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public boolean loadedHistoryFromFile() {
+        try (FileReader reader = new FileReader(file)) {
+            int count;
+            StringBuilder stringBuilder = new StringBuilder();
+            while ((count = reader.read()) != -1) {
+                stringBuilder.append((char) count);
+            }
+            jTextAreaMessages.setText(stringBuilder.toString());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+//            throw new RuntimeException(e);
+            return false;
+        } catch (IOException e) {
+//            throw new RuntimeException(e);
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
     }
 
     public JTextArea getjTextAreaMessages() {
